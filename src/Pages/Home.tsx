32282f2d0bf0cfb5acde0d701dc0759de48efa36
Faridmech <feridh189@gmail.com>
2023-07-27 17:59:03 +0400

    Home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,20 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Center, Image, Text } from "@chakra-ui/react";
 import React from "react";
 
 export const Home: React.FC = () => {
   return (
-    <Box backgroundImage="url('/public/adnsu.png')" width="40px" height="40px">
-      Home
-    </Box>
+    <Center>
+      <Box
+        marginTop="3rem"
+        display="flex"
+        justifyContent="space-between"
+        width="700px"
+        height="120px"
+      >
+        <Image src="/adnsu.png"></Image>
+        <Image src="/Area.png"></Image>
+        <Image src="/aztu.png" bg="rgb(19,29,100)"></Image>
+      </Box>
+    </Center>
   );
 };
