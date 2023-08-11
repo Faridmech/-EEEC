b2920e28eef74f56fdc1a233efd9bde205a5dd41
Faridmech <feridh189@gmail.com>
2023-08-11 17:06:49 +0400

    language

--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -8,12 +8,13 @@ import {
   Tooltip,
   VStack,
   Link as ChakraLink,
-  LinkProps,
+  Select,
 } from "@chakra-ui/react"
 import React from "react"
 import { NAVS } from "./const"
 import { useIsActiveRoute } from "./hooks"
 import { Image } from "@chakra-ui/react"
+import { GrUserAdmin } from "react-icons/gr"
 
 export const Navigations: React.FC = () => {
   const { checkIsActive } = useIsActiveRoute()
@@ -22,82 +23,102 @@ export const Navigations: React.FC = () => {
     <HStack
       zIndex={10}
       w="100%"
-      spacing="20px"
       borderRadius="5px"
       p="5px"
       bg="rgb(245,245,245)"
       pos="sticky"
-      top="5px"
-      alignItems="center"
+      flexDirection="column"
+      alignContent="space-between"
     >
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        width="67px"
-        height="30px"
-        bg="rgb(0,49,119)"
-        borderRadius="5px"
-      >
-        <Tooltip label="ELECTROENERGETICS, ELECTROTECHNICS, ELECTROMECHANICS + CONTROL">
-          <Text
-            color="white"
-            fontWeight="bold"
+      <Box display="flex" flexDirection="row" justifyContent="space-between">
+        <Box
+          display="flex"
+          alignItems="center"
+          width="67px"
+          height="30px"
+          bg="rgb(0,49,119)"
+          borderRadius="5px"
+        >
+          <Tooltip label="ELECTROENERGETICS, ELECTROTECHNICS, ELECTROMECHANICS + CONTROL">
+            <Text
+              color="white"
+              fontWeight="bold"
+              fontSize="1.6rem"
+              margin="0"
+              padding="0"
+            >
+              EEEC
+            </Text>
+          </Tooltip>
+        </Box>
+        <Box display="flex" gap="13px" alignItems="center">
+          <ChakraLink href="http://asoiu.edu.az/">
+            <Tooltip
+              label=" Azerbaijan State Oil and Industry University"
+              hasArrow
+            >
+              <Image
+                marginLeft="1rem"
+                src="/adnsu.png"
+                borderRadius="10%"
+                height={{ lg: "45px", md: "35px", sm: "35px" }}
+              ></Image>
+            </Tooltip>
+          </ChakraLink>
+          <ChakraLink href=" http://azenerji.com/en/about/pei.html">
+            <Tooltip
+              hasArrow
+              label="Azerbaijan Scientific-Research & Design Prospecting Power Engineering Institute"
+            >
+              <Image
+                src="/Area.png"
+                borderRadius="10%"
+                height={{ lg: "45px", md: "35px", sm: "35px" }}
+              ></Image>
+            </Tooltip>
+          </ChakraLink>
+          <ChakraLink href="https://www.aztu.edu.az/en">
+            <Tooltip label="Azerbaijan Technical University" hasArrow>
+              <Image
+                borderRadius="10%"
+                src="/aztu.png"
+                bg="rgb(19,29,100)"
+                height={{ lg: "45px", md: "35px", sm: "35px" }}
+                width="80px"
+              ></Image>
+            </Tooltip>
+          </ChakraLink>
+        </Box>
+
+        <Box display="flex" flexDirection="row" gap="10px" alignItems="center">
+          <Box>
+            <Select>
+              <option value="en">🇺🇸 English</option>
+              <option value="az">🇦🇿 Azərbaycan</option>
+            </Select>
+          </Box>
+          <Box
+            width="40px"
+            height="40px"
+            bg="rgb(200,200,200)"
             fontSize="1.6rem"
-            margin="0"
-            padding="0"
+            borderRadius="50%"
+            cursor="pointer"
+            pt="4px"
+            pl="9px"
           >
-            EEEC
-          </Text>
-        </Tooltip>
+            <GrUserAdmin />
+          </Box>
+        </Box>
       </Box>
-      <Box display="flex" gap="13px" position="sticky" zIndex={200} flex={1}>
-        <ChakraLink href="http://asoiu.edu.az/">
-          <Tooltip
-            label=" Azerbaijan State Oil and Industry University"
-            hasArrow
-          >
-            <Image
-              marginLeft="1rem"
-              src="/adnsu.png"
-              borderRadius="10%"
-              height={{ lg: "45px", md: "35px", sm: "35px" }}
-            ></Image>
-          </Tooltip>
-        </ChakraLink>
 
-        <ChakraLink href=" http://azenerji.com/en/about/pei.html">
-          <Tooltip
-            hasArrow
-            label="Azerbaijan Scientific-Research & Design Prospecting Power Engineering Institute"
-          >
-            <Image
-              src="/Area.png"
-              borderRadius="10%"
-              height={{ lg: "45px", md: "35px", sm: "35px" }}
-            ></Image>
-          </Tooltip>
-        </ChakraLink>
-
-        <ChakraLink href="https://www.aztu.edu.az/en">
-          <Tooltip label="Azerbaijan Technical University" hasArrow>
-            <Image
-              borderRadius="10%"
-              src="/aztu.png"
-              bg="rgb(19,29,100)"
-              height={{ lg: "45px", md: "35px", sm: "35px" }}
-              width="80px"
-            ></Image>
-          </Tooltip>
-        </ChakraLink>
-      </Box>
-      <Box display="flex" flexDirection="row" gap="15px" flex={2}>
+      <Box display="flex" flexDirection="row" gap="15px">
         {NAVS.map((nav) => {
           const isActive = checkIsActive(nav.to)
           return (
             <Link style={{ cursor: "pointer" }} key={nav.to} to={nav.to}>
               <VStack
-                fontSize={{ lg: "1.3rem", md: "0.9rem", sm: "0.9rem" }}
+                fontSize={{ lg: "1.2rem", md: "0.9rem", sm: "0.7rem" }}
                 spacing="0"
                 transition="0.2s ease-in"
                 color={isActive ? "blue.400" : "rgb(0,59,90)"}
